801fd2814dc1bf39d07c0a49ada56f0c30e3759f
ivandiaz2425 <ivandiaz2422@gmail.com>
2018-12-01 16:52:46 -0500

    Firebase Config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyCTF5Ay3GYgEX4jKUf1BC12aLb-ycsz40w",
+    authDomain: "museospasto.firebaseapp.com",
+    databaseURL: "https://museospasto.firebaseio.com",
+    projectId: "museospasto",
+    storageBucket: "museospasto.appspot.com",
+    messagingSenderId: "993715883856"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyCTF5Ay3GYgEX4jKUf1BC12aLb-ycsz40w",
+    authDomain: "museospasto.firebaseapp.com",
+    databaseURL: "https://museospasto.firebaseio.com",
+    projectId: "museospasto",
+    storageBucket: "museospasto.appspot.com",
+    messagingSenderId: "993715883856"
+  }
 };
